b2d9a7accfaa6e2488edc3a6207b00dba5e11dfe
xiriu <scharcanski@gmail.com>
2017-06-27 15:11:27 -0400

    script to convert the depth channel of an exr file to npy

---
+++ mycode/exr_normals_to_npy.m
@@ -0,0 +1,32 @@
+%% EXR_NORMALS_TO_NPY: converts exr containing normals to a numpy file
+% usage: B=EXR_NORMALS_TO_NPY(exr_filename)          % default method
+% 
+% Take an exr file and loads it using the EnvironmentMap lib to save
+% it as npy using the npy-matlab converter
+%
+% arguments (exr_filename):
+%   A - string containing the HOLE name of the file (including its
+%   location)
+%
+% arguments (output):
+%   none
+%
+%
+% Author: Henrique Weber
+% e-mail address: henrique.weber.1@ulaval.ca
+% Release: 0
+% Release date: 06/07/17
+
+function exr_normals_to_npy(exr_filename)
+
+%load normal map
+exr_normal_map = EnvironmentMap(exr_filename,EnvironmentMapFormat.LatLong,'clampData',false);
+
+exr_normal_map_data = exr_normal_map.data;
+exr_normal_map_data(:,:,1) = exr_normal_map_data(:,:,1)*-1;
+exr_normal_map_data(:,:,3) = exr_normal_map_data(:,:,3)*-1;
+
+%save as npy
+writeNPY(exr_normal_map_data, [exr_filename(1:end-4),'.npy']);
+
+end
